dc0381bd46881365097a6ec747b818797d810e32
Ashin Habeeb <ashinchulli@gamill.com>
2024-11-05 09:09:32 +0530

    final version of gemini clone

---
+++ src/config/gemini.js
@@ -0,0 +1,36 @@
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyB4U4r0qcvQtK6c-2jPTCbqNFhx5mj9Fpc";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text()
+  }
+  
+  export default run
